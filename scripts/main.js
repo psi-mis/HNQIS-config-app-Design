9f35c6e41e33241bec6639ba120e49441d893ffd
kimbalear <kimbalear@gmail.com>
2021-08-02 12:51:33 +0200

    lis_program_stages.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     component_bar_slctr('program');
     $(".cont_prg").on("click", function () {
-        console.log('xx')
+        goToURL('lis_program_stages');
      });
+
+     function goToURL(myPath) {
+         console.log(myPath);
+        location.href = '../'+ myPath + '.html';
+      }
 });
